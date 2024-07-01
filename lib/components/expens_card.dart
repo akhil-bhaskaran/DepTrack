34fb679d0528c9f4038aad7727643c12a78ec9de
Shibin-Oh-Yes <shibinsuresh74@gmail.com>
2024-06-30 17:52:02 -0700

    Ui chages:history page

--- lib/components/expens_card.dart
+++ lib/components/expens_card.dart
@@ -15,21 +15,61 @@ class ExpenseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 5),
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+      padding: const EdgeInsets.symmetric( horizontal: 15),
+      margin: const EdgeInsets.symmetric(vertical: 5),
       height: 60,
       width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: const Color.fromARGB(255, 255, 251, 0)),
+        border: Border.all(width: 2, color: Colors.black12),
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Text(day,style: const TextStyle(fontSize: 17),),
               Text(date),
-              Text(day),
+              
             ],
           ),
+          Text("- $amount \u{20B9}",style: TextStyle(color: Colors.green[900],fontSize: 20),)
+        ],
+      ),
+    );
+  }
+}
+
+//New card like widget for better understand payments
+
+class PayedCard extends StatelessWidget {
+  const PayedCard({super.key, required this.payedmoney});
+  final String payedmoney;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      width: double.maxFinite,
+      height: 40,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.green[100],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            "Payed",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+           payedmoney,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
+          )
         ],
       ),
     );
--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -12,16 +12,34 @@ class History extends StatelessWidget {
     return Consumer<MenuProvider>(
       builder: (context, value, child) => Scaffold(
           appBar: AppBar(
+            leading: IconButton(onPressed: (){
+              Navigator.pop(context);
+            }, icon: const Icon(Icons.arrow_back_ios)),
             backgroundColor: Colors.transparent,
             title: Text(
-              "H I S T O R Y ",
+              "History",
               style: Values.txtstyle4,
             ),
           ),
-          body: ListView.builder(
-            itemCount: 4,
-            itemBuilder: (context, index) => ExpenseCard(
-                date: '1/1/2024', day: "Moday", amount: value.todaysExpense),
+          body: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+           child: ListView(
+            children: const [
+              PayedCard(payedmoney: '119'),
+              ExpenseCard(date: '01/05/2024', day: 'Monday', amount: 26),
+              ExpenseCard(date: '31/04/2024', day: 'Friday', amount: 28),
+              ExpenseCard(date: '30/04/2024', day: 'Thursday', amount: 34),
+              ExpenseCard(date: '29/04/2024', day: 'Wednesday', amount: 31),
+            ],
+           ),
+           
+            // child: ListView.builder(
+            //   itemCount: 4,
+            //   itemBuilder: (context, index) => ExpenseCard(
+            //       date: '1/1/2024', day: "Moday", amount: value.todaysExpense),
+            // ),
           )),
     );
   }
