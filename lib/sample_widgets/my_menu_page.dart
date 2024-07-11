40e4b73368bf5bf245044c01887fef119d823348
Shibin-Oh-Yes <shibinsuresh74@gmail.com>
2024-07-10 17:27:02 -0700

    update:Menu page

--- lib/sample_widgets/my_menu_page.dart
+++ lib/sample_widgets/my_menu_page.dart
@@ -19,32 +19,56 @@ class MyMenuPage extends StatelessWidget {
               color: ColorPattern.rd,
             ),
             Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
                 width: double.maxFinite,
                 height: MediaQuery.of(context).size.height * .7078,
                 decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius:
                         BorderRadius.vertical(top: Radius.circular(50))),
-                child: const Column())
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.more_horiz)),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.delete))
+                      ],
+                    ),
+                    const Text("\u{20B9} 0.0",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                    const SizedBox(height: 10,),
+                  ],
+                ))
           ],
         ),
         Positioned(
-          top: (MediaQuery.of(context).size.height * .2) - 50,
+          top: (MediaQuery.of(context).size.height * .2) - 45,
           right: 0,
           left: 0,
           child: Container(
-            decoration:  BoxDecoration(
-              border: Border.all(color: Colors.white,width: 5),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 5),
               shape: BoxShape.circle,
-              color:ColorPattern.rd,
+              color: ColorPattern.rd,
             ),
-            width: 100,
-            height: 100,
+            width: 90,
+            height: 90,
             child: Center(
               child: IconButton(
                   onPressed: () {
                     log("button pressed");
-                  }, icon: const Icon(Icons.verified,color: Colors.white,size: 40,)),
+                  },
+                  icon: const Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 50,
+                  )),
             ),
           ),
         ),
